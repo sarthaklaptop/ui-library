70df3008036a5276c9451c96a4d320f4b59d8706
sarthaklaptop <sarthaklaptop402@gmail.com>
2024-08-15 12:28:10 +0530

    Updated eslint and tsconfigurations

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,8 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 })
