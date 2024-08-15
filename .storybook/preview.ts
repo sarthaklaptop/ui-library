57d4f99d67e05677545f4977e1de3d1e84ee36b3
sarthaklaptop <sarthaklaptop402@gmail.com>
2024-08-15 13:07:53 +0530

    Add Eslint and StoryBook configuration

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+
+// link storybook to tailwind css
+import "../src/index.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
-export const Button = () => {
+import { ComponentProps } from "react"
+
+type ButtonProps = ComponentProps<"button">;
+
+export const Button = ({...props}: ButtonProps) => {
   return (
-    <button className="text-blue-500">Button</button>
+    <button className="text-blue-500" {...props} />
   )
 }
